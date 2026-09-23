e981780e6bc10b7a615706570883c015cd39ac01
powerfannetwork-hub <powerfannetwork@gmail.com>
2026-09-23 14:14:50 +0100

    Add GemType enum with extensions for emoji and labels
    Defines an enum for GemType with associated emoji and nameLabel extensions.

---
+++ lib/models/gem_type.dart
@@ -0,0 +1,59 @@
+enum GemType {
+  fire,
+  water,
+  nature,
+  storm,
+  light,
+  void,
+}
+
+extension GemTypeExtension on GemType {
+  String get emoji {
+    switch (this) {
+      case GemType.fire:
+        return '🔥';
+
+      case GemType.water:
+        return '💧';
+
+      case GemType.nature:
+        return '🌿';
+
+      case GemType.storm:
+        return '⚡';
+
+      case GemType.light:
+        return '✨';
+
+      case GemType.void:
+        return '🌌';
+    }
+  }
+
+  String get nameLabel {
+    switch (this) {
+      case GemType.fire:
+        return 'Fire';
+
+      case GemType.water:
+        return 'Water';
+
+      case GemType.nature:
+        return 'Nature';
+
+      case GemType.storm:
+        return 'Storm';
+
+      case GemType.light:
+        return 'Light';
+
+      case GemType.void:
+        return 'Void';
+    }
+  }
+
+  static GemType fromIndex(int index) {
+    return GemType.values[
+        index % GemType.values.length];
+  }
+}
