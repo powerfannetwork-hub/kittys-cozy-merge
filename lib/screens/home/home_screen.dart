7638ebc67c55451fb90b035e837e4d24bd511f56
powerfannetwork-hub <powerfannetwork@gmail.com>
2026-09-23 13:38:36 +0100

    Add import for main_navigation_screen.dart

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../core/theme/app_theme.dart';
+import 'screens/main_navigation_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
