b3a1f35509e155bd5df22b107fb291b5b61f6736
powerfannetwork-hub <powerfannetwork@gmail.com>
2026-09-25 16:25:21 +0100

    Replace AppPlaceholder with HomeScreen as home widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'core/theme/app_theme.dart';
+import 'screens/home/home_screen.dart';
 import 'services/game_storage_service.dart';
 
 Future<void> main() async {
@@ -20,26 +21,7 @@ class KittysCozyMergeApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Kitty's Cozy Merge",
       theme: AppTheme.light(),
-      home: const AppPlaceholder(),
-    );
-  }
-}
-
-class AppPlaceholder extends StatelessWidget {
-  const AppPlaceholder({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Kitty's Cozy Merge",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
